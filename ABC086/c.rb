b4ed514b307cfd499f7d195c83c6b2e5b595d1c8
nari19 <kazuki7793996@gmail.com>
2019-03-03 07:18:02 +0900

    0303

---
+++ ABC086/c.rb
@@ -0,0 +1,12 @@
+# Traveling     https://atcoder.jp/contests/abc086/tasks/arc089_a
+
+n = gets.to_i
+data = n.times.map{gets.strip.split.map(&:to_i)}
+
+p data
+
+if 
+  puts 'Yes'
+else
+  puts 'No'
+end
