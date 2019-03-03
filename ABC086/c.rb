9acf5314248a5eed5e540639e8d3af847e9d7381
nari19 <kazuki7793996@gmail.com>
2019-03-04 08:11:06 +0900

    0304

--- ABC086/c.rb
+++ ABC086/c.rb
@@ -3,10 +3,19 @@
 n = gets.to_i
 data = n.times.map{gets.strip.split.map(&:to_i)}
 
-p data
+result = 'Yes'
 
-if 
-  puts 'Yes'
-else
-  puts 'No'
+n.times do |i|
+    if i == 0 then plus = (data[i][1]).abs + (data[i][2]).abs
+    else plus = (data[i][1] - data[i-1][1]).abs + (data[i][2] - data[i-1][2]).abs end
+
+    hoge = (data[i][0] - data[i-1][0]).abs
+
+    x = plus > hoge
+    y = hoge.odd? && plus.abs.even?
+    z = hoge.even? && plus.abs.odd?
+    
+    result = 'No' if x || y || z
 end
+
+puts result
