ff95b3ace0fd9fbf9716ad9449e6e82ea443d663
nari19 <kazuki7793996@gmail.com>
2019-03-09 18:21:05 +0900

    bar

--- ABC084/c.rb
+++ ABC084/c.rb
@@ -7,4 +7,26 @@ csf = (n-1).times.map{ gets.strip.split.map(&:to_i) }
 # [n][1]  s     s秒後に発車
 # [n][2]  f     f秒おきに発車
 
-p csf
+# 3
+# 6 5 1
+# 1 10 1
+
+# 12
+# 11
+# 0
+
+puts "============"
+n.times do |i|
+    if i == n-2
+        result = csf[i][0] + csf[i][1]
+    elsif i == n-1
+        result = 0 
+    else
+        n-i-1.times do |j|
+            bar =  csf[j][0] + csf[j][1] 
+            baz = (bar - csf[j+1][1]) / csf[j+1][2]
+            result = bar + baz + csf[j+1][0]
+        end
+    end
+    puts result
+end
