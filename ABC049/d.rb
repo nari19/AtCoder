315fa7804a32c77b19aa4a6fbcc5a55ac82583c9
nari19 <kazuki7793996@gmail.com>
2019-03-12 08:46:25 +0900

    0312

---
+++ ABC049/d.rb
@@ -0,0 +1,12 @@
+# Connectivity       https://atcoder.jp/contests/abc049/tasks/arc065_b
+
+n, k, l = gets.strip.split.map(&:to_i)
+p = (k + l).each{ gets.strip.split.map(&:to_i) }
+puts "==============="
+
+
+
+
+
+puts n, k, l
+p p
