a48f351c0ac9783c8361d90e9e55725a2f13390b
nari19 <kazuki7793996@gmail.com>
2019-03-12 08:31:33 +0900

    0312

--- ABC088/c.rb
+++ ABC088/c.rb
@@ -3,8 +3,25 @@
 c = 3.times.map{ gets.strip.split.map(&:to_i) }
 puts "==========="
 
-def aaa()
+result = "No"
 
-end
+# c[0][0] == x + u
+# c[0][1] == x + v
+# c[0][2] == x + w
 
-puts a == b ? "Yes" : "Np"
+# c[1][0] == y + u
+# c[1][1] == y + v
+# c[1][2] == y + w
+
+# c[2][0] == z + u
+# c[2][1] == z + v
+# c[2][2] == z + w
+
+hoge = c[0][0] + c[1][1] + c[2][2]
+fuga = c[0][1] + c[1][2] + c[2][0]
+piyo = c[0][2] + c[1][0] + c[2][1]
+
+result = "Yes" if hoge == fuga && fuga == piyo
+        
+
+p result
