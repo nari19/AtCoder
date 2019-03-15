06c7ae273c3d768372fdf8e22d03f0888ada1cd8
nari19 <kazuki7793996@gmail.com>
2019-03-15 09:34:47 +0900

    0315

--- ABC050/b.rb
+++ ABC050/b.rb
@@ -1,3 +1,13 @@
 # Contest with Drinks Easy      https://atcoder.jp/contests/abc050/tasks/abc050_b
 
+n = gets.to_i
+t = gets.strip.split.map(&:to_i)
+m = gets.to_i
+p = m.times.map{ gets.strip.split.map(&:to_i) }
+puts "================"
+
+m.times do |i|
+    s = p[i][0] - 1
+    puts t.sum + p[i][1] - t[s]
+end
 
