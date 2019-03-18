756ed75c9e255b72c0c1175042d5f7cc1d3071fa
nari19 <kazuki7793996@gmail.com>
2019-03-19 08:48:43 +0900

    0319

---
+++ ABC051/a.rb
@@ -0,0 +1,7 @@
+#Haiku       https://atcoder.jp/contests/abc051/tasks/abc051_a
+
+s = gets.split(",").map(&:strip)
+puts "======="
+
+puts s.join(" ")
+
---
+++ ABC051/b.rb
@@ -0,0 +1,8 @@
+# Sum of Three Integers       https://atcoder.jp/contests/abc051/tasks/abc051_b
+
+k, s = gets.strip.split.map(&:to_i)
+
+
+0 <= x || 0 <= y || 0 <= z || x <= k || y <= k || z <= k
+x + y + z == s
+
