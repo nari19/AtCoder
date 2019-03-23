639cef8dfbef5c5c3cd9c3447a84b9d023b4b3a8
nari19 <kazuki7793996@gmail.com>
2019-03-23 09:26:18 +0900

    0323

--- ABC052/b.rb
+++ ABC052/b.rb
@@ -1,7 +1,16 @@
 # Increment Decrement       https://atcoder.jp/contests/abc052/tasks/abc052_b
 
 n = gets.to_i
-x = gets.to_i
+s = gets.strip.split("")
 puts "============"
 
+x = 0
+result = 0
 
+for var in s
+    x += 1 if var == "I"
+    x -= 1 if var == "D"
+    result = x if result < x
+end
+
+puts result
