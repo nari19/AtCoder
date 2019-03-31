0362654a19ea0523a7ff2c4fc5234a74d035d850
nari19 <kazuki7793996@gmail.com>
2019-04-01 07:39:37 +0900

    0401

--- ABC055/b.rb
+++ ABC055/b.rb
@@ -1,3 +1,13 @@
 # Training Camp     https://atcoder.jp/contests/abc055/tasks/abc055_b
 
+n = gets.to_i
+puts "================"
 
+foo = (10 ** 9) + 7
+result = 1
+(1..n).each do |i|
+    result %= foo    
+    result *= i
+end
+
+puts result % foo
