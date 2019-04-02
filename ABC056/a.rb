e7065c6af2335ec0e436b47c4b1be627133ec810
nari19 <kazuki7793996@gmail.com>
2019-04-03 08:56:22 +0900

    0403

---
+++ ABC056/a.rb
@@ -0,0 +1,15 @@
+# HonestOrDishonest     https://atcoder.jp/contests/abc056/tasks/abc056_a
+
+a, b = gets.strip.split
+puts "================="
+
+
+result = ""
+if a == b
+    result = "H"
+else
+    result = "D"
+end
+
+
+puts result
---
+++ ABC056/b.rb
@@ -0,0 +1,6 @@
+# NarrowRectanglesEasy      https://atcoder.jp/contests/abc056/tasks/abc056_b
+
+w, a, b = gets.strip.split.map(&:to_i)
+puts "================="
+
+puts w, a, b
