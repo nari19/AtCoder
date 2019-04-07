336e9f480ffbe37c2243430c495912b340178445
nari19 <kazuki7793996@gmail.com>
2019-04-07 14:00:29 +0900

    0407

--- ABC057/b.rb
+++ ABC057/b.rb
@@ -1,9 +1,20 @@
 # Checkpoints       https://atcoder.jp/contests/abc057/tasks/abc057_b
 
 n, m = gets.strip.split.map(&:to_i)
-a, b = n.times.map{ gets.strip.split.map(&:to_i) }.transpose
-c, d = m.times.map{ gets.strip.split.map(&:to_i) }.transpose
+nab = n.times.map{ gets.strip.split.map(&:to_i) }
+mcd = m.times.map{ gets.strip.split.map(&:to_i) }
 puts "====================="
 
-puts "#{n}, #{m}"
-p a, b, c, d
+
+n.times do |i|
+    foo = 10 ** 8
+    result = []
+    for var in mcd do
+        baz = (var[0] - nab[i][0]).abs + (var[1] - nab[i][1]).abs
+        if  baz < foo then
+            foo = baz
+            result = var
+        end
+    end
+    puts mcd.index(result) + 1
+end
