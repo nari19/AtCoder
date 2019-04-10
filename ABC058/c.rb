2e93b0cd6a57b1c381f43b1a5fb114793fb92594
nari19 <kazuki7793996@gmail.com>
2019-04-11 08:45:54 +0900

    0411

---
+++ ABC058/c.rb
@@ -0,0 +1,12 @@
+# 怪文書 / Dubious Document     https://atcoder.jp/contests/abc058/tasks/abc058_b
+
+n = gets.to_i
+s = n.times.map{ gets.strip }
+
+result = []
+i = 0
+for var in s do
+    foo = var.split("").uniq.sort
+    if result[i]
+    i += 1
+end
