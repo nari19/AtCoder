96aebe31c1a9af6589d92437a8ea2996a6185080
nari19 <kazuki7793996@gmail.com>
2019-04-16 08:20:58 +0900

    0416

--- ABC059/b.rb
+++ ABC059/b.rb
@@ -1,3 +1,12 @@
 # Comparison        https://atcoder.jp/contests/abc059/tasks/abc059_b
 
+a = gets.to_i
+b = gets.to_i
+
+result = "EQUAL"
+result = "GREATER" if a > b
+result = "LESS" if a < b
+
+
+puts result
 
---
+++ ABC059/c.rb
@@ -0,0 +1,7 @@
+# Sequence      https://atcoder.jp/contests/abc059/tasks/arc072_a
+
+n = gets.to_i
+a = gets.strip.split(&:to_i)
+
+
+
