bf712c74a191ec797804a4e1987b5fece09e0dc0
nari19 <kazuki7793996@gmail.com>
2019-04-25 10:29:29 +0900

    0425

--- ABC061/c.rb
+++ ABC061/c.rb
@@ -2,6 +2,13 @@
 
 n, k = gets.strip.split.map(&:to_i)
 a, b = n.times.map{ gets.strip.split.map(&:to_i) }.transpose
+puts "===================="
 
-puts n, k
-p a, b
+
+result = []
+(0...n).each do |i|
+    b[i].times{ result.push(a[i]) }
+end
+
+
+p result[k -1]
