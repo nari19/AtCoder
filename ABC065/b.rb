da6827a4247ce4877d670e699b99d8ed1e9dc328
nari19 <kazuki7793996@gmail.com>
2019-05-01 09:55:27 +0900

    0501

--- ABC065/b.rb
+++ ABC065/b.rb
@@ -2,9 +2,14 @@
 
 n = gets.strip.to_i
 a = n.times.map{ gets.to_i }
+puts "====================="
 
-puts n
-p a
+result = []
+t = 0
+a.length.times do
+    result.push(a[t])
+    break if a[t] == 2
+    t = a[t] -1
+end
 
-
-dfkjhvldkfhbl
+puts result.last != 2 ? -1 : result.length
