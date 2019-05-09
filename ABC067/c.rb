b669b68fb1d5ae064e967673b0a91bfa71910260
nari19 <kazuki7793996@gmail.com>
2019-05-09 10:17:36 +0900

    0509

--- ABC067/c.rb
+++ ABC067/c.rb
@@ -1,7 +1,21 @@
 # Splitting Pile        https://atcoder.jp/contests/abc067/tasks/arc078_a
 
 n = gets.strip.to_i
-x = gets.strip.to_i
+x = gets.strip.split.map(&:to_i)
 puts "==========="
 
-puts n * x
+foo = []
+hn = n / 2
+(1..hn).each do |i|
+    foo.push(x.permutation(i).to_a)
+end
+
+result = x.sort.last
+for var in foo do
+    for baz in var do
+        hoge = x.sum - (baz.sum * 2)
+        result = hoge.abs if hoge.abs < result
+    end
+end
+
+puts result
