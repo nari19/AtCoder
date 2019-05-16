0052b3097b5a83901029d9b44a7849a3bdb81b17
nari19 <kazuki7793996@gmail.com>
2019-05-16 09:35:06 +0900

    0516

--- ABC071/b.rb
+++ ABC071/b.rb
@@ -1,2 +1,11 @@
 # Not Found     https://atcoder.jp/contests/abc071/tasks/abc071_b
 
+s = gets.strip.split("")
+puts "================="
+
+foo = "abcdefghijklmnopqrstuvwxyz".split("")
+for var in s do
+    foo.reject!{ |a| a == var }
+end
+
+puts foo == [] ? "None" : foo.first
