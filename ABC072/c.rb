c39d8b88a6946ae59abcd3bf47483910bb623ef7
nari19 <kazuki7793996@gmail.com>
2019-05-20 09:54:02 +0900

    0520

---
+++ ABC072/c.rb
@@ -0,0 +1,12 @@
+# Together      https://atcoder.jp/contests/abc072/tasks/arc082_a
+
+n = gets.strip.to_i
+a = gets.strip.split.map(&:to_i)
+puts "=========================-"
+
+am = a.map{ |n| n-1 }
+ap = a.map{ |m| m+1 }
+foo = a + am + ap
+bar = foo.max_by{ |x| foo.count(x) }
+
+puts foo.count(bar)
