8e1f06fd05b2ffcc4f631af679ae339a885114a7
nari19 <kazuki7793996@gmail.com>
2019-05-23 08:13:08 +0900

    0523

--- ABC073/b.rb
+++ ABC073/b.rb
@@ -1,3 +1,12 @@
 # Theater       https://atcoder.jp/contests/abc073/tasks/abc073_b
 
+n = gets.strip.to_i
+l, r = n.times.map{ gets.split.map(&:to_i) }.transpose
+puts "======================"
 
+foo = []
+(0...n).each do |i|
+    foo.push(r[i] - l[i] + 1)
+end
+
+puts foo.sum
