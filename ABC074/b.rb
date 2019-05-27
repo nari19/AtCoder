56bd0dbc4e0cea7444ac592ce0cffef1b6cbf392
nari19 <kazuki7793996@gmail.com>
2019-05-27 14:24:33 +0900

    0527

--- ABC074/b.rb
+++ ABC074/b.rb
@@ -2,6 +2,14 @@
 
 n = gets.to_i
 k = gets.to_i
+x = gets.strip.split.map(&:to_i)
 puts "================="
 
-puts n, k
+result = 0
+n.times do |i|
+    ra = x[i] * 2
+    rb = (k - x[i]).abs * 2
+    result += ra > rb ? rb : ra
+end
+
+puts result
