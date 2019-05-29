8d33d08bb62c99cbd2f43ae4f2ca0c82511dd62d
nari19 <kazuki7793996@gmail.com>
2019-05-29 11:57:36 +0900

    0529

--- ABC074/c.rb
+++ ABC074/c.rb
@@ -1,3 +1,16 @@
 # Sugar Water       https://atcoder.jp/contests/abc074/tasks/arc083_a
 
+a, b, c, d, e, f = gets.strip.split.map(&:to_i)
+puts "====================="
 
+water = 0
+sugar = 0
+
+ccr = (10 * suger) / (water + suger)
+melt = (water / 100) * e
+
+while (water + suger) < f do
+
+end
+
+puts 
