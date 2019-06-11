14af4e66c1dabb1edfbbe41f86fe72271ce12ca7
nari19 <kazuki7793996@gmail.com>
2019-06-11 13:23:12 +0900

    0611

--- ABC076/c.rb
+++ ABC076/c.rb
@@ -1,6 +1,12 @@
 # Dubious Document      https://atcoder.jp/contests/abc076/tasks/abc076_c
 
-s = gets.to_i
-t = gets.to_i
+s = gets.strip
+t = gets.strip
 puts "==========="
 
+foo = s.split("")
+foo.each do |i| 
+    if i != "?" then
+        puts "#{foo.index(i) + 1}番目 #{i}"
+    end
+end
