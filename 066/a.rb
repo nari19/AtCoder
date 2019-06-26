cff0b16a396fc9459529a42f034ce83f704c38f1
nari19 <kazuki7793996@gmail.com>
2019-06-26 10:12:33 +0900

    0626

--- 066/a.rb
+++ 066/a.rb
@@ -1,9 +1,3 @@
-# ringring      https://atcoder.jp/contests/abc066/tasks/abc066_a
-
-a, b, c = gets.strip.split.map(&:to_i)
-puts " ============"
-
-result = [a, b, c]
-result.sort!
+result = gets.split.map(&:to_i).sort
 
 puts result[0] + result[1]
--- 066/b.rb
+++ 066/b.rb
@@ -1,23 +1,30 @@
-# ss        https://atcoder.jp/contests/abc066/tasks/abc066_b
+# s = gets.strip.split("")
 
-s = gets.strip.split("")
-puts "==============="
+# result = 0
+# s.length.times do
+#     s.pop(1)
+#     foo = s.length / 2
+#     bar = 0
+#     (1..foo).each do |i|
+#         fuga = s.join.scan(/.{1,#{i}}/)
+#         for var in fuga do
+#             puts var
+#             hoge = fuga.index(var) + 1
+#             bar = 1 if var != hoge && var != fuga.last
+#         end
+#         break if bar == 0
+#     end
+#     break if bar == 0
+# end
 
-result = 0
-s.length.times do
-    s.pop(1)
-    foo = s.length / 2
-    bar = 0
-    (1..foo).each do |i|
-        fuga = s.join.scan(/.{1,#{i}}/)
-        for var in fuga do
-            puts var
-            hoge = fuga.index(var) + 1
-            bar = 1 if var != hoge && var != fuga.last
-        end
-        break if bar == 0
-    end
-    break if bar == 0
+# puts s.length
+
+s = gets.split("")
+while s.length > 0 do
+   s.delete_at(-1)
+   hoge = s.join.scan(/.{1,#{s.length/2}}/)
+   p hoge
+   break if hoge[0] == hoge[1]
 end
 
 puts s.length
