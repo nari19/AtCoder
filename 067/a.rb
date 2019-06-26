541c743c37a766f8c652480dff1785ed7494681f
nari19 <kazuki7793996@gmail.com>
2019-06-27 08:16:41 +0900

    0627

--- 067/a.rb
+++ 067/a.rb
@@ -1,8 +1,3 @@
-# Sharing Cookies       https://atcoder.jp/contests/abc067/tasks/abc067_a
-
 a, b = gets.strip.split.map(&:to_i)
-puts "================="
-
 foo = a % 3 == 0 || b % 3 == 0 || (a + b) % 3 == 0
-
 puts foo ? "Possible" : "Impossible"
