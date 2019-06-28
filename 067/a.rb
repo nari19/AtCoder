404995fbb89ab2c63350cf33dc41d46012c39e81
nari19 <kazuki7793996@gmail.com>
2019-06-28 09:22:37 +0900

    0628

--- 067/a.rb
+++ 067/a.rb
@@ -1,3 +1,3 @@
-a, b = gets.strip.split.map(&:to_i)
+a, b = gets.split.map(&:to_i)
 foo = a % 3 == 0 || b % 3 == 0 || (a + b) % 3 == 0
 puts foo ? "Possible" : "Impossible"
--- 067/b.rb
+++ 067/b.rb
@@ -1,10 +1,4 @@
-# Snake Toy     https://atcoder.jp/contests/abc067/tasks/abc067_b
+n, k = gets.split.map(&:to_i)
+i = gets.split.map(&:to_i).sort
 
-n, k = gets.strip.split.map(&:to_i)
-i = gets.strip.split.map(&:to_i)
-puts "================"
-
-i.sort!
-foo = n - k
-i.shift(foo)
-puts i.sum
+p i[n-k..-1].inject(:+)
