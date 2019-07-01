5866a31c7329ba7c4a04978b60854e72cf363dd7
nari19 <kazuki7793996@gmail.com>
2019-07-01 10:01:58 +0900

    0701

--- 070/a.rb
+++ 070/a.rb
@@ -1,6 +1,2 @@
-# Palindromic Number        https://atcoder.jp/contests/abc070/tasks/abc070_a
-
-n = gets.strip.to_s
-puts "=========="
-
+n = gets.strip
 puts n == n.split("").reverse.join ? "Yes" : "No"
--- 070/b.rb
+++ 070/b.rb
@@ -1,7 +1,1 @@
-# Two Switches      https://atcoder.jp/contests/abc070/tasks/abc070_b
-
-a, b, c, d = gets.strip.split.map(&:to_i)
-puts "=========="
-
-foo = b - c - a
-puts foo > 0 ? foo : 0
+a, b, c, d = gets.split.map(&:to_i)
