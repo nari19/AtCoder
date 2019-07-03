d55934466166f0db6bddd7df9e89e6d3eff8dc88
nari19 <kazuki7793996@gmail.com>
2019-07-03 10:08:50 +0900

    0703

--- 071/a.rb
+++ 071/a.rb
@@ -1,7 +1,2 @@
-# MealDlivery       https://atcoder.jp/contests/abc071/tasks/abc071_a
-
-x, a, b = gets.strip.split.map(&:to_i)
-puts "================"
-
-foo = (x - a).abs > (x - b).abs
-puts foo ? "B" : "A"
+x, a, b = gets.split.map(&:to_i)
+puts (x - a).abs > (x - b).abs ? "B" : "A"
--- 071/b.rb
+++ 071/b.rb
@@ -1,11 +1,6 @@
-# Not Found     https://atcoder.jp/contests/abc071/tasks/abc071_b
-
 s = gets.strip.split("")
-puts "================="
 
 foo = "abcdefghijklmnopqrstuvwxyz".split("")
-for var in s do
-    foo.reject!{ |a| a == var }
-end
+s.each{ |e| foo.reject!{ |a| a == e } }
 
 puts foo == [] ? "None" : foo.first
