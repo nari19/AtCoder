d910004c434f947741d08c7bc5bc6a000d3e6372
nari19 <kazuki7793996@gmail.com>
2019-07-15 08:38:57 +0900

    0715

--- 082/a.rb
+++ 082/a.rb
@@ -1,7 +1,2 @@
-# Round Up the Mean     https://atcoder.jp/contests/abc082/tasks/abc082_a
-
-a, b = gets.strip.split.map(&:to_f)
-puts "=================="
-
-foo = (a + b) / 2
+foo = gets.split.map(&:to_f).inject(:+) / 2
 puts foo%1==0 ? foo.to_i : (foo + 1).to_i
--- 082/b.rb
+++ 082/b.rb
@@ -1,11 +1,3 @@
-# Two Anagrams      https://atcoder.jp/contests/abc082/tasks/abc082_b
-
-s, t = 2.times.map{ gets.chomp! }
-puts "=================="
-def sort_join(string)
-    string.split("").sort.join
-end
-
-puts "#{sort_join(s)} : #{sort_join(t).reverse}"
-puts sort_join(s) < sort_join(t).reverse ? 'Yes' : 'No'
+s, t = 2.times.map{ gets.chomp!.split("").sort.join }
+puts s < t.reverse ? 'Yes' : 'No'
 
