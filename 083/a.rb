163f3b68cdd4dd379b0eb20f19600c7def91bd18
nari19 <kazuki7793996@gmail.com>
2019-07-16 09:32:56 +0900

    0716

--- 083/a.rb
+++ 083/a.rb
@@ -1,11 +1,2 @@
-# Libra     https://atcoder.jp/contests/abc083/tasks/abc083_a
-
-a, b, c, d = gets.strip.split(' ').map(&:to_i)
-
-if a+b < c+d
-    puts 'Right'
-elsif a+b > c+d
-    puts 'Left'
-else
-    puts 'Balanced'
-end
+a, b, c, d = gets.split.map(&:to_i)
+puts (a+b>c+d) ? 'Left' : (a+b==c+d) ? 'Balanced' : 'Right'
--- 083/b.rb
+++ 083/b.rb
@@ -1,16 +1,7 @@
-# Sume Sums     https://beta.atcoder.jp/contests/abc083/tasks/abc083_b
-
-x, a, b = gets.strip.split.map(&:to_i)
+n, a, b = gets.split.map(&:to_i)
 hoge = 0
-
-for i in 1..x do
-  sum = i/10 + i%10
-  if a <= sum && sum <= b
-      puts i
-      hoge += i
-  end
+(1..n).each do |i|
+  sum = i.to_s.split("").map(&:to_i).inject(:+)
+  hoge += i if a <= sum && sum <= b
 end
-
-puts "========================="
 puts hoge
-
