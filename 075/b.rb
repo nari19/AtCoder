9cc7d930b2d86d98faf6f97d552001570913d3d7
nari19 <kazuki7793996@gmail.com>
2019-07-24 09:34:34 +0900

    0724

--- 075/b.rb
+++ 075/b.rb
@@ -1,21 +1,8 @@
 h, w = gets.split.map(&:to_i)
 s = h.times.map{ gets.strip.split("") }
-
-h.times do |i|
-    w.times do |j|
-        if s[i][j] == "." then
-            s[i][j] = "0"
-            s[i][j].succ! if i != 0 && s[i-1][j] == "#"
-            s[i][j].succ! if i != 0 && j != w-1 && s[i-1][j+1] == "#"
-            s[i][j].succ! if i != 0 && j != 0 && s[i-1][j-1] == "#"
-            s[i][j].succ! if j != w-1 && s[i][j+1] == "#"
-            s[i][j].succ! if i != 0 && s[i][j-1] == "#"
-            s[i][j].succ! if i != h-1 && s[i+1][j] == "#"
-            s[i][j].succ! if i != h-1 && j != w-1 && s[i+1][j+1] == "#"
-            s[i][j].succ! if i != h-1 && j != 0 && s[i+1][j-1] == "#"
-        end
+h.times{ |i| w.times{ |j|
+    if s[i][j] == "."
+        s[i][j] = s[[0, i - 1].max..i + 1].flat_map{ |t| t[[0, j - 1].max..j + 1] }.count("#")
     end
-end
-
+}}
 puts s.map(&:join)
-
