2291b84bb0327e933eaa31918041bb0a524eb22d
nari19 <kazuki7793996@gmail.com>
2019-07-27 10:26:25 +0900

    0727

--- 087/c.rb
+++ 087/c.rb
@@ -1,18 +1,11 @@
- # Candies  https://antcoder.jp/contests/abc087/tasks/arc090_a
-
 n = gets.to_i
-a = 2.times.map{ gets.strip.split.map(&:to_i) }
-puts "==============="
+a = 2.times.map{ gets.split.map(&:to_i) }
 
 result = 0
 (1..n).each do |i|
     baz = 0
-    i.times do |j|
-        baz += a[0][j]
-    end
-    (i..n).each do |k|
-        baz += a[1][k-1]
-    end
+    i.times{ |j| baz += a[0][j] }
+    (i..n).each{ |k| baz += a[1][k-1] }
     puts "#{i} : #{baz}"
     result = baz if baz > result
 end
