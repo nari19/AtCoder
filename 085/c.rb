bcf8353be7bc38979cd0cc5bb442e645d413442b
nari19 <kazuki7793996@gmail.com>
2019-07-29 09:41:25 +0900

    0729

--- 085/c.rb
+++ 085/c.rb
@@ -1,14 +1,7 @@
-#  Otoshidama   https://atcoder.jp/contests/abc085/tasks/abc085_c
-
-x, y  = gets.strip.split(' ').map(&:to_i)
+x, y  = gets.split.map(&:to_i)
 _i, _j, _k = -1, -1, -1
 
-(0..x).each do |i|
-    (0..x-i).each do |j|
-        if y == 10000*i + 5000*j + 1000*(x-i-j)
-            _i, _j, _k = i, j, x-i-j
-        end
-    end
-end
-
+(0..x).each{ |i| (0..x-i).each{ |j| 
+    _i, _j, _k = i, j, x-i-j if y == 10000*i + 5000*j + 1000*(x-i-j)
+}}
 puts "#{_i} #{_j} #{_k}"
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - [AtCoder Problems](https://kenkoooo.com/atcoder/?user=&rivals=&kind=category#/user/nari19)
 
+
 - [AtCoder on Ruby](https://qiita.com/d_nishiyama85/items/f79e034f6dcd4175cdc1)
 
 - [Rubyで競プロするときのTips](https://betrue12.hateblo.jp/entry/2018/12/01/224748)
