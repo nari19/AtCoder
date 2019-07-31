cd7e32b9eeccd255c7da56d010169b4d123dcc64
nari19 <kazuki7793996@gmail.com>
2019-07-31 09:18:17 +0900

    0731

--- 083/c.rb
+++ 083/c.rb
@@ -1,15 +1,8 @@
-# Multiple Gift     https://atcoder.jp/contests/abc083/tasks/arc088_a
-
-x, y = gets.strip.split.map(&:to_i)
-
+x, y = gets.split.map(&:to_i)
 i = 0
 array = [x]
 while array[i] < y do
     i += 1
     array[i] = array[i-1] * 2
 end
-
-p array
 puts (array.length - 1)
-
-
