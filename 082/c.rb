e7be328ed5ac576534c6bb6e35489792ae465ba9
nari19 <kazuki7793996@gmail.com>
2019-08-01 09:59:26 +0900

    0801

--- 082/c.rb
+++ 082/c.rb
@@ -1,14 +1,8 @@
-# Good Sequence     https://atcoder.jp/contests/abc082/tasks/arc087_a
-
 n = gets.to_i
-a = gets.strip.split.map(&:to_i)
-
-puts "=========="
+a = gets.split.map(&:to_i)
 result = 0
-
 for var in a.sort.uniq
     baz = (var - a.count(var))
     result += (baz > 0 ? a.count(var) : baz.abs)
 end
-
 puts result
