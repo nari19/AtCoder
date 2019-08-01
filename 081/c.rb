cc001036e994f0f7286b3e6bb8a3c4cdd5050281
nari19 <kazuki7793996@gmail.com>
2019-08-02 07:49:07 +0900

    0802

--- 081/c.rb
+++ 081/c.rb
@@ -1,10 +1,5 @@
-#  Not so Diverse       https://atcoder.jp/contests/abc081/tasks/arc086_a
+n, k = gets.split.map(&:to_i)
+input = gets.split.map(&:to_i)
 
-n, k = gets.strip.split.map(&:to_i)
-input = gets.strip.split.map(&:to_i)
-
-result = 0
 len = input.uniq.length
-
-result = (len - k) if len > k
-puts result
+puts len > k ? (len - k) : 0
