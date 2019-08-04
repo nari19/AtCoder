ed8431d2bd7f3a083e082f510ccbd1597bf772a9
nari19 <kazuki7793996@gmail.com>
2019-08-04 17:36:58 +0900

    -8-4

--- 050/c.rb
+++ 050/c.rb
@@ -1,21 +1,12 @@
-# Lining Up     https://atcoder.jp/contests/abc050/tasks/arc066_a
-
 n = gets.to_i
-a = gets.strip.split.map(&:to_i)
-puts "=================="
+a = gets.split.map(&:to_i)
 
 result = 0
 foo = ((n/2)**2) % (10 ** 9 + 7)
 
 bar = n.odd? ? (0...n).step(2).to_a : (1...n).step(2).to_a
-p bar
 baz = bar.concat(bar)
 baz.delete_at(baz.find_index(0)) if n.odd?
-p "a: #{a.sort}"
-p a.sort
 result = foo if baz.sort == a.sort
   
 puts result 
-
-
-
