b90c44dccf08d2b8408d5273e642d5b1338dc9fa
nari19 <kazuki7793996@gmail.com>
2019-08-05 08:14:35 +0900

    0805

--- 051/c.rb
+++ 051/c.rb
@@ -1,7 +1,2 @@
-# Back and Forth        https://atcoder.jp/contests/abc051/tasks/abc051_c
-
-sx, sy, tx, ty = gets.strip.split.map(&:to_i)
-
+sx, sy, tx, ty = gets.split.map(&:to_i)
 puts sx, sy, tx, ty
-
-
