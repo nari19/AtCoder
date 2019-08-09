0c3cc019773c0fe66c7fb4db01cb8fdb48781ed8
nari19 <kazuki7793996@gmail.com>
2019-08-09 23:47:47 +0900

    0809

--- 053/c.rb
+++ 053/c.rb
@@ -1,22 +1,7 @@
-# X: Yet Another Die Game       https://atcoder.jp/contests/abc053/tasks/arc068_a
-
-# 1 2~5
-# 2 6~11
-# 3 12~
-
 x = gets.to_i
-puts "============"
 
 foo = (x % 11)
-bar = 0
-
-if foo < 5
-    bar = 1
-else
-    bar = 2
-end
-
-
+bar = foo < 5 ? 1 : 2
 puts (x / 11 * 2) + bar
 
 
