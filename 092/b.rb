0700a429972f70716fb273caea17af41ecb08aaa
nari19 <kazuki7793996@gmail.com>
2019-09-26 08:12:06 +0900

    0926

--- 092/b.rb
+++ 092/b.rb
@@ -1,3 +1,16 @@
 n = gets.to_i
 d, x = gets.split.map(&:to_i)
 a = n.times.map{ gets.to_i }
+
+sum = 0
+a.each do |i|
+    d.times do |j|
+        # hoge = (i * j) + 1
+        # puts "---------"
+        # puts hoge
+        # break hoge > d
+        # sum += 1
+        puts "#{i}, #{j}"
+    end
+end
+puts sum + x
