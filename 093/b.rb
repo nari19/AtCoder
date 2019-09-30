4cfbf4290f2532a7de1efe9c0783a73fc203b220
nari19 <kazuki7793996@gmail.com>
2019-09-30 12:12:10 +0900

    0930

--- 093/b.rb
+++ 093/b.rb
@@ -1,4 +1,2 @@
 a, b, k = gets.split.map(&:to_i)
-hoge = (a..b).to_a
-hoge = (a..(a+k-1)).to_a + ((b-k+1)..b).to_a if hoge.length - (2 * k) > 0
-puts hoge
+puts (b-a).succ - (2*k) > 0 ? (a..(a+k-1)).to_a + ((b-k+1)..b).to_a : (a..b).to_a
