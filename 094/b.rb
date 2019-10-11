56c266bbaa84082280db67d4e25b79ab75ad277f
nari19 <kazuki7793996@gmail.com>
2019-10-11 11:11:27 +0900

    1011

--- 094/b.rb
+++ 094/b.rb
@@ -1,6 +1,4 @@
 n, m, x = gets.split.map(&:to_i)
 a = gets.split.map(&:to_i)
 
-hoge = a.push(x).index(x).succ
-fuga = a.length - hoge
-puts (a.length / 2) > fuga ? (hoge - 1) : fuga
+puts a.partition{ |i| i > x}.map(&:size).min
