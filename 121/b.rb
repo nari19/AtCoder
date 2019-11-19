8a54c2a00860456ad1f94ae9fec73b91fff65900
nari19 <kazuki7793996@gmail.com>
2019-11-19 13:18:58 +0900

    1119

--- 121/b.rb
+++ 121/b.rb
@@ -1,6 +1,4 @@
-n, m, c = gets.split.map(&:to_i)
-b = gets.split.map(&:to_i)
-a = n.times.map{ gets.split.map(&:to_i) }
-
-p b
-p a
+def hoge() gets.split.map(&:to_i) end
+n, m, c = hoge
+b = hoge
+a = n.times.map{ hoge }
