940c4404a316fdc0cae6aaddc4407e2ce5a522c3
nari19 <kazuki7793996@gmail.com>
2019-12-05 16:34:05 +0900

    1205

--- 103/b.rb
+++ 103/b.rb
@@ -1,16 +1,4 @@
-s, t = 2.times.map{ gets.strip.split("") }
-
-flag = 0
-if s.sort == t.sort
-    t.count(s[0]).times do |i|
-        ss = t.index(s[0])
-        s.length.times do |j|
-            sss = ss.succ > t.length ? 0 : ss.succ
-            if s[j] != t[sss] then flag = 1; break end
-        end
-        break if flag == 1
-    end
-    
-end
-
-puts flag==1 ? "Yes" : "No"
+s = gets.chomp
+t = gets.chomp
+str =s << s
+puts str.include?(t) ? "Yes" : "No"
