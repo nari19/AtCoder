0d7799757c25d3856bd5866f164c51bcc9bc2398
nari19 <kazuki7793996@gmail.com>
2019-12-10 14:14:59 +0900

    1210

--- 104/b.rb
+++ 104/b.rb
@@ -1,3 +1,3 @@
-str = gets.chomp.split("")
+str = gets.strip.split("")
 hoge = (str[2..str.count - 2].count("C") == 1) && (str.join.count('A-Z') == 2)
 puts str[0] == "A" ? (hoge ? "AC" : "WA") : "WA"
