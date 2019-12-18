c8264cf50b7ceab4d62fba62e81dc597340a035a
nari19 <kazuki7793996@gmail.com>
2019-12-18 21:14:17 +0900

    1218

--- 107/b.rb
+++ 107/b.rb
@@ -1,2 +1,17 @@
 h, w = gets.split.map(&:to_i)
-a = h.times.map{ gets }
+a = h.times.map{ gets.split("") }
+
+hei, wid = [], []
+
+a.each_with_index{ |x, y| wid.push(y) if x.count(".") == w }
+
+w.times do |i|
+    hoge = 1
+    a.each{ |j| hoge = 0 if j[i] == "#" }
+    hei.push(i) if hoge == 1
+end
+
+h.times do |i|
+    w.times{ |j| print a[i][j] if !hei.include?(j) && !wid.include?(i) }
+    puts "\n" if !wid.include?(i)
+end
