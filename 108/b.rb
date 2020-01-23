323af60c03903d9e0dfd1dee0941e44c100bfd31
nari19 <kazuki7793996@gmail.com>
2020-01-23 13:36:48 +0900

    0123

--- 108/b.rb
+++ 108/b.rb
@@ -1,3 +1,9 @@
-xy = gets.split.map(&:to_i)
-hoge = (xy[3] - xy[1])
-puts "#{xy[2]-hoge} #{xy[3]} #{xy[0]-hoge} #{xy[1]}"
+a,b,c,d = gets.split.map(&:to_i)
+m, n = (c - a), (d - b)
+
+e = c - n
+f = d + m
+g = e - m
+h = f - n
+
+puts [e,f,g,h].join(" ")
