f2ed4dbc7c48ff774cf5888e43bac3b917454f0b
nari19 <kazuki7793996@gmail.com>
2020-02-01 14:44:49 +0900

    0201

--- 110/b.rb
+++ 110/b.rb
@@ -1,2 +1,12 @@
 def hoge() gets.split.map(&:to_i) end
 n, m, x, y = hoge; xx = hoge; yy = hoge
+
+yyy, xxx = yy.sort[0], xx.sort.reverse[0]
+flag = false
+if xxx <= yyy
+    (xxx.succ..yyy).to_a.each do |i| 
+        if i > x && i <= y then flag = true; break end
+    end
+end
+
+puts flag ? "No War" : "War"
