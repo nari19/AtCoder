d5f9557fa7a081c2466e794dc5d8376d69199937
nari19 <kazuki7793996@gmail.com>
2020-03-01 12:57:18 +0900

    0301

--- 11x/117/b.ts
+++ 11x/117/b.ts
@@ -1,8 +1,7 @@
 ((input: string): void => {
     const hoge: string[] = input.split(/\n/);
     const l: number[] = hoge[1].split(" ").map(Number);
-    l.sort((a, b) => { return a-b });
-    const fuga: number = l.slice(0, -1).reduce((a, b) => a+b, 0);
-    const piyo: boolean = l.slice(-1)[0] < fuga;
-    console.log(piyo ? 'Yes' : 'No');
+    l.sort((a: number, b: number): number => { return a-b });
+    const fuga: number = l.slice(0, -1).reduce((a: number, b: number) => a+b, 0);
+    console.log(l.slice(-1)[0] < fuga ? 'Yes' : 'No');
 })(require('fs').readFileSync('/dev/stdin', 'utf8'));
