efbb51d57b6165adaeff9abc547be2a644bd0114
nari19 <kazuki7793996@gmail.com>
2020-03-15 09:19:58 +0900

    0315

--- 12x/125/b.ts
+++ 12x/125/b.ts
@@ -1,6 +1,7 @@
 ((input: string): void => {
-    const hoge: number[][] = input.trim().split(/\n/).map(
+    const [_, v, c]: number[][] = input.trim().split(/\n/).map(
         v => v.split(" ").map(Number)
-    )
-    console.log(hoge);
+    );
+    const hoge = v.map( (v, i) => v - c[i] ).filter(v => v > 0)
+    console.log(hoge.length==0 ? 0 : hoge.reduce( (a, b) => a+b ));
 })(require('fs').readFileSync('/dev/stdin', 'utf8'));
