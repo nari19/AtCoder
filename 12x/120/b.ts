63cfb6c57b6140098ee4f88cfd2169222ff3a5b7
nari19 <kazuki7793996@gmail.com>
2020-04-01 19:45:35 +0900

    0401

--- 12x/120/b.ts
+++ 12x/120/b.ts
@@ -1,7 +1,7 @@
 ((input: string): void => {
     const [a, b, k] = input.split(" ").map(Number);
     const hoge: number = a>b ? b : a;
-    const fuga = [...Array(hoge/2).keys()].map(i => ++i)
-    const piyo = [...fuga, hoge]
-    console.log(piyo);
+    const fuga = [...Array(hoge).keys()].map(i => ++i)
+    const piyo = fuga.filter(v => a%v==0 && b%v==0)
+    console.log(piyo.slice(-k)[0])
 })(require('fs').readFileSync('/dev/stdin', 'utf8'));
