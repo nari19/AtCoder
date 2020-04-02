647df272a939a5a4fac99fc8d445b4ea52ac14fd
nari19 <kazuki7793996@gmail.com>
2020-04-02 19:40:35 +0900

    0402

--- 12x/123/a.ts
+++ 12x/123/a.ts
@@ -1,5 +1,5 @@
 ((input: string): void => {
-    const hoge: number[] = input.trim().split(/\n/).map(Number)
-    const fuga: number = hoge.slice(-2)[0] - hoge[0];
+    const hoge = input.trim().split(/\n/).map(Number)
+    const fuga = hoge.slice(-2)[0] - hoge[0];
     console.log(fuga > hoge.slice(-1)[0] ? ':(' : 'Yay!');
 })(require('fs').readFileSync('/dev/stdin', 'utf8'));
