a3dba04c41c6a1dde7062653ac074c4a2cbc872b
nari19 <kazuki7793996@gmail.com>
2020-05-06 09:46:35 +0900

    0506

--- 13x/137/b.ts
+++ 13x/137/b.ts
@@ -1,4 +1,5 @@
 ((input: string): void => {
-    const [a, b] = input.split(" ").map(Number)
-    console.log(Math.max(a+b, a-b, a*b));
+   const [k, x] = input.split(" ").map(Number)
+   const hoge = [...Array((2*k)-1)].map((_,i) => x-k+1+i)
+   console.log(hoge.join(" "));
 })(require('fs').readFileSync('/dev/stdin', 'utf8'));
