51b6a0bc24312d32af1877e3b5bd8c5989afdaf1
k.narizuka <kazuki7793996@gmail.com>
2021-01-13 19:24:01 +0900

    0113

--- 14x/140/b.js
+++ 14x/140/b.js
@@ -1,3 +1,11 @@
 ((input) => {
-  console.log(input.split('\n').map(v => v.split(" ")))
+  const dish = input.split('\n').slice(0, -1)
+    .map(v => v.split(" "))
+    .map((v) => v.map((_v) => Number(_v)))
+  const satisfactionLevels = [...Array(dish[0][0])].map( (_,i) => {
+    const num = dish[1][i]
+    const lev = dish[2][num-1]
+    return (dish[0][0]-1 != i && num+1 == dish[1][i+1]) ? lev + dish[3][num-1] : lev
+  })
+  console.log(satisfactionLevels.reduce((a,b) => a+=b, 0))
 })(require('fs').readFileSync("/dev/stdin", 'utf8').slice(0, -1))
