73cd12f8142ede7b00c27ddd783696cf64f45786
k.narizuka <kazuki7793996@gmail.com>
2021-01-18 16:34:24 +0900

    0118

--- 11x/118/b.js
+++ 11x/118/b.js
@@ -1,6 +1,11 @@
 (input => {
-  const hoge = input.trim().split('\n').map( v => {
-    return v.split(" ").map(Number)
+  const hoge = input.trim().split('\n').map( (v,i) => {
+    const foo = v.split(" ").map(Number)
+    return i==0 ? foo : foo.slice(1)
   })
-  console.log(hoge)
+  const fuga = hoge[1].concat(hoge[2]).concat(hoge[3])
+  const piyo = [...Array(hoge[0][0])].map( (_,i) => i+1).filter( v => {
+    return fuga.filter(_v=> _v==v).length == hoge[0][0]
+  })
+  console.log(hoge[0][0]==1 ? hoge[1].length : piyo.length)
 })(require('fs').readFileSync('/dev/stdin', 'utf8').slice(0, -1));
