74a12ee7ed295c03785aa704e5473e69210ee164
k.narizuka <kazuki7793996@gmail.com>
2021-02-13 09:52:13 +0900

    0213

--- 14x/143/b.js
+++ 14x/143/b.js
@@ -1,22 +1,11 @@
-(input => {
-  const [N, d] = input.split('\n').map(v => v.split(" ").map(Number))
-  // https://tech-blog.s-yoshiki.com/entry/144
-  const combination = (nums, k) => {
-    let ans = [];
-    if (nums.length < k) return []
-    if (k === 1) {
-      [...Array(nums.length)].forEach((_,i) => ans[i] = [nums[i]])
-    } else {
-      [...Array(nums.length - k + 1)].forEach((_,i) => {
-        let row = combination(nums.slice(i + 1), k - 1);
-        [...Array(row.length)].forEach((_,j) => {
-          ans.push([nums[i]].concat(row[j]))
-        })
-      })
-      return ans;
-    }
-  }
-  let arr = combination(['a', 'b', 'c'], 2)
-  console.log(JSON.stringify(arr))
-
+( input => {
+  const [_, d] = input.split('\n').map(v => v.split(" ").map(Number))
+  let sum = 0;
+  [...Array(d.length)].forEach( (_,i) => {
+    [...Array(d.length)].forEach( (_,j) => {
+      if (i === j)  return true
+      sum += d[i] * d[j];
+    })
+  })
+  console.log(sum / 2);
 })(require('fs').readFileSync("/dev/stdin", 'utf8'))
