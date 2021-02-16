0bf9ec499e34de8fefc9f38c16dab7d23e1a8de2
k.narizuka <kazuki7793996@gmail.com>
2021-02-16 14:00:39 +0900

    0216

--- 13x/135/c.js
+++ 13x/135/c.js
@@ -1,4 +1,15 @@
 (input => {
   const [N, A, B] = input.split('\n').map(v => v.split(" ").map(Number))
-  console.log(N, A, B);
+  let surplus = 0
+  const [_A, _B] = [A.concat(), B.concat()]
+  B.forEach((v,i) => {
+    if (_A[i] > _B[i]) {
+      _A[i] = _A[i] - _B[i]
+      _B[i] = 0
+    } else {
+      _A[i] = 0
+      _B[i] = _B[i] - _A[i]
+    }
+  })
+  console.log(_A, _B);
 })(require('fs').readFileSync("/dev/stdin", 'utf8'))
