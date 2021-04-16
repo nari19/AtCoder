3bf2e33c366e5bb81b9b586b3892fc40b34d7cb8
nari19 <kazuki7793996@gmail.com>
2021-04-17 00:45:00 +0900

    0417

--- 15x/157/b.js
+++ 15x/157/b.js
@@ -1,5 +1,5 @@
 (input => {
-  const [A, N, b] = (sInp => {
+  const [A, _, b] = (sInp => {
     return [...Array(3)].map((_,i) => {
       return  {
         0: [sInp[0], sInp[1], sInp[2]].map(v => v.split(" ")),
@@ -8,5 +8,27 @@
       }[i]
     })
   })(input.split('\n'))
-  console.log(A, N, b)
+  const bingo = A.map(v => v.map(_v => {
+    return b.includes(_v) ? -1 : _v
+  }))
+  const comb = [
+    "000102",
+    "101112",
+    "202122",
+    "001020",
+    "011121",
+    "021222",
+    "001122",
+    "022220",
+  ]
+  comb.some((v,i) => {
+    const [a,b,c,d,e,f] = v.split("")
+    const bi = (x,y) => bingo[x][y] === -1
+    if (bi(a,b) && bi(c,d) && bi(e,f)) {
+      console.log("Yes")
+      return true
+    } else if(i===7) {
+      console.log("No")
+    }
+  })
 })(require('fs').readFileSync("/dev/stdin", 'utf8'))
